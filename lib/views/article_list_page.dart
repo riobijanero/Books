b418566d8fe70c2d61de0ec23143941003951ae8
Bijan Sellahi <bisevilua.net@Bijans-MacBook-Pro.local>
2020-09-06 17:16:42 +0200

    add article list page

---
+++ lib/views/article_list_page.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../common/constants/style_constants.dart';
+
+import '../enums/connectivity_status.dart';
+import '../interfaces/i_connectivity_service.dart';
+import '../services/connectivity_service.dart';
+import '../view_models/article_list_view_model.dart';
+import '../widgets/SearchButton.dart';
+import '../widgets/article_list.dart';
+import '../widgets/searchbar.dart';
+
+class ArticleListPage extends StatefulWidget {
+  @override
+  _ArticleListPageState createState() => _ArticleListPageState();
+}
+
+class _ArticleListPageState extends State<ArticleListPage> {
+  TextEditingController _textEditingController;
+  ArticleListViewModel _viewModel;
+  ConnectivityService _connectivityService;
+
+  bool isLoading = false;
+  String searchTerm;
+  String finishedSearchTerm;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _viewModel = Provider.of<ArticleListViewModel>(context);
+    _connectivityService = Provider.of<IConnectivityService>(context);
+  }
+
+  void showSnackBar(BuildContext context) {
+    final snackBar = SnackBar(content: Text('no Internet connection'));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  onSearchButtonPressed(String value, BuildContext context) async {
+    ConnectivityStatus connectionInfo =
+        await _connectivityService.connectionInfo;
+    if (connectionInfo == ConnectivityStatus.Offline) {
+      showSnackBar(context);
+    } else if (!isLoading && value != null && value.isNotEmpty) {
+      setState(() {
+        isLoading = true;
+      });
+      await _viewModel.fetchArticles(value);
+      _textEditingController.clear();
+      setState(() {
+        isLoading = false;
+        finishedSearchTerm = value;
+      });
+      searchTerm = "";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(finishedSearchTerm != null
+            ? 'Search results for \"$finishedSearchTerm\"'
+            : 'Articles'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                Flexible(
+                  flex: 4,
+                  child: Builder(
+                    builder: (context) => SearchBar(
+                      searchBarHeight: searchBarHeight,
+                      textEditingController: _textEditingController,
+                      onSubmitted: (value) async =>
+                          await onSearchButtonPressed(value, context),
+                      onChanged: (value) => setState(() {
+                        searchTerm = value;
+                      }),
+                    ),
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Builder(
+                    builder: (context) => SearchButton(
+                      searchBarHeight: searchBarHeight,
+                      isLoading: isLoading,
+                      onSearchButtonPressed: () async =>
+                          await onSearchButtonPressed(searchTerm, context),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: _viewModel.articleList == null
+                  ? Center(child: Text(_viewModel.errorMessage))
+                  : ArticleList(articleList: _viewModel.articleList),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
